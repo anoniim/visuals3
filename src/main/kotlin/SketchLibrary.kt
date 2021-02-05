a8663aa011ed17d7afd575b43ce6cfe6a36a15c6
anoniim <marcel.broz@gmail.com>
2021-02-05 20:11:57 +0100

    Reordered sketch library

--- src/main/kotlin/SketchLibrary.kt
+++ src/main/kotlin/SketchLibrary.kt
@@ -1,55 +1,58 @@
 import processing.core.PApplet
-import sketch.lines.ConstrainedNoiseWithStencil
-import sketch.patterns.MaurerRosePatterns
-import sketch.physics.LivingNetworkGraph
-import sketch.sequences.CollatzConjecture
-import sketch.sound.BeatDetectorClock
-import sketch.sound.miso.EveningExperiment1Idea2
-import sketch.squares.RaisingTiles
 
 @ExperimentalStdlibApi
 fun main() {
-//    PApplet.main(arrayOf(FocusedSquares::class.qualifiedName))
-    PApplet.main(arrayOf(RaisingTiles::class.qualifiedName))
-//    PApplet.main(arrayOf(PixelatedWalkers::class.qualifiedName))
-//    PApplet.main(arrayOf(SmoothWalkers::class.qualifiedName))
-//    PApplet.main(arrayOf(HyperbolicPlane::class.qualifiedName))
-//    PApplet.main(arrayOf(CirclePacking::class.qualifiedName))
-//    PApplet.main(arrayOf(Bubbles::class.qualifiedName))
+
+    // Practice
 //    PApplet.main(arrayOf(CircleLining::class.qualifiedName))
 //    PApplet.main(arrayOf(SkippingLines::class.qualifiedName))
-//    PApplet.main(arrayOf(RoundingLines::class.qualifiedName))
-//    PApplet.main(arrayOf(Dot::class.qualifiedName))
-//    PApplet.main(arrayOf(SoupParticles::class.qualifiedName))
+//    PApplet.main(arrayOf(CirclePacking::class.qualifiedName))
+//    PApplet.main(arrayOf(Bubbles::class.qualifiedName))
 //    PApplet.main(arrayOf(Starfield::class.qualifiedName))
+//    PApplet.main(arrayOf(StripyBlobLoop::class.qualifiedName))
+//    PApplet.main(arrayOf(WaterRipples::class.qualifiedName))
+//    PApplet.main(arrayOf(FireEffect::class.qualifiedName))
+//    PApplet.main(arrayOf(CollatzConjecture::class.qualifiedName))
+//    PApplet.main(arrayOf(TimesTablesCircle::class.qualifiedName))
+//    PApplet.main(arrayOf(MoireLines::class.qualifiedName))
+//    PApplet.main(arrayOf(BarVisualizer::class.qualifiedName))
+
+    // Tools
+//    PApplet.main(arrayOf(Dot::class.qualifiedName))
+//    PApplet.main(arrayOf(RosePatterns::class.qualifiedName))
+//    PApplet.main(arrayOf(EveningExperiment1Visualizer::class.qualifiedName))
+
+    // Ideas
+//    PApplet.main(arrayOf(HyperbolicPlane::class.qualifiedName))
+//    PApplet.main(arrayOf(FocusedSquares::class.qualifiedName))
+//    PApplet.main(arrayOf(SquareSpiral::class.qualifiedName))
+//    PApplet.main(arrayOf(SpinningSpiral::class.qualifiedName))
+//    PApplet.main(arrayOf(HalfFill::class.qualifiedName))
+//    PApplet.main(arrayOf(SineFlower::class.qualifiedName))
+//    PApplet.main(arrayOf(CurlingCircles::class.qualifiedName))
+
+    // Showcase
+//    PApplet.main(arrayOf(PixelatedWalkers::class.qualifiedName))
+//    PApplet.main(arrayOf(SmoothWalkers::class.qualifiedName))
+//    PApplet.main(arrayOf(RaisingTiles::class.qualifiedName))
+//    PApplet.main(arrayOf(SoupParticles::class.qualifiedName))
 //    PApplet.main(arrayOf(MatrixLetters::class.qualifiedName))
 //    PApplet.main(arrayOf(TextFromRandom::class.qualifiedName))
-//    PApplet.main(arrayOf(StripyBlobLoop::class.qualifiedName))
 //    PApplet.main(arrayOf(NoisyWave::class.qualifiedName))
 //    PApplet.main(arrayOf(NoisyWaveSurface::class.qualifiedName))
 //    PApplet.main(arrayOf(Toothpicks::class.qualifiedName))
-//    PApplet.main(arrayOf(WaterRipples::class.qualifiedName))
-//    PApplet.main(arrayOf(FireEffect::class.qualifiedName))
 //    PApplet.main(arrayOf(RecamansSequence::class.qualifiedName))
 //    PApplet.main(arrayOf(ZenSpiral::class.qualifiedName))
-//    PApplet.main(arrayOf(SquareSpiral::class.qualifiedName))
-//    PApplet.main(arrayOf(SpinningSpiral::class.qualifiedName))
-//    PApplet.main(arrayOf(TimesTablesCircle::class.qualifiedName))
-//    PApplet.main(arrayOf(RosePatterns::class.qualifiedName))
-//    PApplet.main(arrayOf(MaurerRosePatterns::class.qualifiedName))
 //    PApplet.main(arrayOf(AnimatedRosePatterns::class.qualifiedName))
-//    PApplet.main(arrayOf(HalfFill::class.qualifiedName))
-//    PApplet.main(arrayOf(MoireLines::class.qualifiedName))
-//    PApplet.main(arrayOf(MoireHexagons::class.qualifiedName))
+//    PApplet.main(arrayOf(MaurerRosePatterns::class.qualifiedName))
 //    PApplet.main(arrayOf(CircleTangents::class.qualifiedName))
-//    PApplet.main(arrayOf(BarVisualizer::class.qualifiedName))
-//    PApplet.main(arrayOf(BeatDetectorClock::class.qualifiedName))
-//    PApplet.main(arrayOf(SineFlower::class.qualifiedName))
-//    PApplet.main(arrayOf(EveningExperiment1Visualizer::class.qualifiedName))
 //    PApplet.main(arrayOf(EveningExperiment1Idea2::class.qualifiedName))
-//    PApplet.main(arrayOf(CurlingCircles::class.qualifiedName))
 //    PApplet.main(arrayOf(ConstraintNoise::class.qualifiedName))
 //    PApplet.main(arrayOf(ConstrainedNoiseWithStencil::class.qualifiedName))
+
+    // In progress
+//    PApplet.main(arrayOf(RoundingLines::class.qualifiedName))
+//    PApplet.main(arrayOf(MoireHexagons::class.qualifiedName))
+//    PApplet.main(arrayOf(BeatDetectorClock::class.qualifiedName))
 //    PApplet.main(arrayOf(LivingNetworkGraph::class.qualifiedName))
-//    PApplet.main(arrayOf(CollatzConjecture::class.qualifiedName))
 }
