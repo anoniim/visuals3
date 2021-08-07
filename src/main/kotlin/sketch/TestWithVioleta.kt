5b4c488682e156f016db43364fb0076c1fac7368
Marcel Bro <marcel.broz@gmail.com>
2021-08-07 13:21:06 +0100

    Interpolation test with Violeta

---
+++ src/main/kotlin/sketch/TestWithVioleta.kt
@@ -0,0 +1,60 @@
+package sketch
+
+import BaseSketch
+import Screen
+
+class TestWithVioleta: BaseSketch(Screen(800, 400)) {
+
+    override fun setup() {
+    }
+
+    override fun draw() {
+        translate(50f, halfHeightF)
+        background(grey3)
+        drawStart()
+        drawEnd()
+        drawMovingPoint()
+    }
+
+    private val animationLength = 200f
+    private val maxAmt = calculateMaxAmt(animationLength)
+
+    private fun interpolationFormula(animationProgress: Float) = -1 * cos(animationProgress) + 1
+
+    private fun calculateMaxAmt(animationLength: Float): Float {
+        val increment = TWO_PI / animationLength
+        var x = 0f
+        var sum = 0f
+        while (x < TWO_PI) {
+            sum += interpolationFormula(x)
+            x += increment
+        }
+        println("The sum is $sum")
+        return sum
+    }
+
+    private var xProgress = 0f
+    private fun drawMovingPoint() {
+        stroke(red)
+        strokeWeight(10f)
+
+        val animationProgress = map(frameCountF % animationLength, 0f, animationLength, 0f, TWO_PI)
+        xProgress += interpolationFormula(animationProgress)
+
+        val x = map(xProgress, 0f, maxAmt, 0f, 300f)
+        point(x, 0f)
+    }
+
+
+    private fun drawStart() {
+        stroke(grey11)
+        strokeWeight(15f)
+        point(0f, 0f)
+    }
+
+    private fun drawEnd() {
+        stroke(grey11)
+        strokeWeight(15f)
+        point(300f, 0f)
+    }
+}
