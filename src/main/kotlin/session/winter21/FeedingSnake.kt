afee5ffcf6ae72440c04efdaf66881241da55345
Marcel Bro <marcel.broz@gmail.com>
2021-12-25 00:21:05 +0000

    Feeding Snake (WIP)

--- src/main/kotlin/session/winter21/FeedingSnake.kt
+++ src/main/kotlin/session/winter21/FeedingSnake.kt
@@ -13,23 +13,24 @@ fun main() {
 
 class FeedingSnake: BaseSketch() {
 
-    private val dampening = 0.95f
+    private val repelArea = 100f
 
     private val snake = Snake()
     private val midiController by lazy { MidiController(this, 1, 2) }
 
     override fun setup() {
         midiController.on(MidiController.PAD_1..MidiController.PAD_16, { pitch, velocity ->
-            val directionAngle = ((pitch - 20) / 8f) * PI
+            val directionAngle = ((pitch - 12) / 15f) * TWO_PI
             snake.move(directionAngle, velocity.toFloat())
         })
-
     }
+
     override fun draw() {
         translateToCenter()
         background(grey3)
 
         snake.update()
+        snake.edges()
         snake.draw()
     }
 
@@ -39,22 +40,35 @@ class FeedingSnake: BaseSketch() {
         var position = PVector()
         var acceleration = PVector()
         var velocity = PVector()
+        private var size = 10f
 
         @Synchronized
         fun move(angle: Float, strength: Float) {
-            acceleration.add(PVector.fromAngle(angle).mult(strength/100f))
-            velocity.add(acceleration).mult(dampening)
+            acceleration.add(PVector.fromAngle(angle))
+            velocity.add(acceleration).normalize().mult(0.9f)
+            acceleration = PVector()
+            size += (strength - size) * 0.07f
         }
+
 
         fun update() {
             position.add(velocity)
-            segments.add(Segment(position.copy(), 50f))
+            if(frameCount % 4 == 0) segments.add(Segment(position.copy(), size))
         }
 
         @Synchronized
         fun draw() {
             segments.forEach {
                 it.draw()
+            }
+        }
+
+        fun edges() {
+            when {
+                position.x < -halfWidthF + repelArea -> move(HALF_PI, size)
+                position.x > halfWidthF - repelArea -> move(-HALF_PI, size)
+                position.y < -halfHeightF + repelArea -> move(HALF_PI, size)
+                position.y > halfHeightF - repelArea -> move(-HALF_PI, size)
             }
         }
     }
