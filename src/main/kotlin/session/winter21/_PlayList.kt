c614c9a2b1cf9a3deaef3b8d54555aa02afc68e2
Marcel Bro <marcel.broz@gmail.com>
2021-12-27 12:09:34 +0000

    Play list for Winter21 session

---
+++ src/main/kotlin/session/winter21/_PlayList.kt
@@ -0,0 +1,33 @@
+package session.winter21
+
+import processing.core.PApplet
+import sketch.dots.Pointillism
+import sketch.pixels.BrightnessReveal
+import sketch.pixels.ImageFilters
+
+fun main() {
+    // Basic
+//    PApplet.main(CirclePads::class.java)
+//    PApplet.main(PianoKeys::class.java)
+//    PApplet.main(SymmetricLineKeys::class.java)
+
+    // Experiments
+//    PApplet.main(HelixKeys::class.java)
+//    PApplet.main(WaveFieldKeys::class.java)
+//    PApplet.main(FeedingSnake::class.java)
+
+    // Promising
+//    PApplet.main(CircularStrings::class.java)
+//    PApplet.main(StripyKeys2::class.java)
+//    PApplet.main(StripyKeys::class.java)
+//    PApplet.main(TriangleBreakDown::class.java)
+
+    // Image
+//    PApplet.main(RevealingSquares::class.java)
+//    PApplet.main(RevealingStripes::class.java)
+
+    // Image experiments
+//    PApplet.main(BrightnessReveal::class.java)
+//    PApplet.main(Pointillism::class.java)
+//    PApplet.main(ImageFilters::class.java)
+}
