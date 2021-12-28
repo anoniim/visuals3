64a4e303d74ea8cd7b2608eac7d0a33dc21ebbe8
Marcel Bro <marcel.broz@gmail.com>
2021-12-28 21:46:24 +0000

    Revealing stripes update * Vignette opening affect * Stripes revealed in order * Multiple-line stripe reveal effect

--- src/main/kotlin/session/winter21/RevealingStripes.kt
+++ src/main/kotlin/session/winter21/RevealingStripes.kt
@@ -4,49 +4,88 @@ import BaseSketch
 import Screen
 import processing.core.PApplet
 import processing.core.PImage
-import show.first.MidiController
+import input.MidiController
+import kotlin.random.Random
+
 
 fun main() {
     PApplet.main(RevealingStripes::class.java)
 }
 
 class RevealingStripes : BaseSketch(
-//    screen = Screen.EPSON_PROJECTOR
+    screen = Screen.LG_ULTRAWIDE,
 ) {
 
-    // config
-    private val minAlpha = 100f
-    private val numOfSegments = 44
-    private val middleSegment = 21
-    private val revealBackgroundImage = true
-    private val showMiddleSegmentLast = true
-    private val showStripeNumbers = false
+    private val keyRange = MidiController.PAD_24..MidiController.PAD_72
 
-    private val segmentSize by lazy { ceil(widthF / numOfSegments) }
+    // config
+    private val segmentSize = 60
+    private val subsegmentWidth = 20 // 2-10
+    private val maxSubsegmentOffset = 100 // 10-300
+    private val revealProgressSpeed = 10f // 1-10
+    private val middleStrips = listOf(16, 17)
+    private val showStripeNumbers = false
+    private val vignetteSpeed = 4
+    private val maxAlpha = 555f
+    private val maxVignetteFrames = vignetteSpeed * maxAlpha
+
     private val stripes = mutableMapOf<Int, Stripe>()
-    private val background by lazy { loadImage("input/winter21/surfer_scaled.png") } // sunset, cliff
+    private val numOfSegments by lazy { ceil(widthF / segmentSize) }
+    private val segmentOrder by lazy { generateSegmentOrder() }
+    private var segmentPointer = 0
+    private val originalImage by lazy { loadImage("input/winter21/surfer_scaled.png") }
+    private val background by lazy { originalImage.copy() }
+    private val vignetteIndexMiddle by lazy { width * height / 2f }
     private val controller by lazy { MidiController(this, 1, 2) }
+    private val isRevealed
+        get() = segmentPointer >= segmentOrder.size
 
     override fun setup() {
         super.setup()
+        MidiController.printDevices()
         controller.on(
-            MidiController.PAD_1..MidiController.PAD_16,
+            keyRange,
             triggerAction = { pitch, velocity ->
                 generateNewStripe()
             })
-        background.resize(width, 0)
-    }
-
-    override fun draw() {
-        background(grey3)
-
-        updateStripes()
+        originalImage.resize(width, 0)
+        background.filter(BLUR, 50f)
     }
 
     override fun keyTyped() {
         if (key == ' ') {
             generateNewStripe()
         }
+    }
+
+    override fun draw() {
+        drawBlurredBackground()
+        applyVignette()
+        updateStripes()
+    }
+
+    private fun drawBlurredBackground() {
+        if (isRevealed) return
+        image(background, 0f, 0f)
+    }
+
+    private fun applyVignette() {
+        if (frameCount > maxVignetteFrames) return
+        val alphaOffset = vignetteSpeed * frameCountF
+        val vignette = createImage(width, height, ARGB)
+        vignette.loadPixels()
+        for (i in vignette.pixels.indices) {
+            val alpha = map(
+                abs(vignetteIndexMiddle - i),
+                0f,
+                vignetteIndexMiddle,
+                255f - alphaOffset,
+                maxAlpha - alphaOffset
+            )
+            vignette.pixels[i] = color(255f, alpha)
+        }
+        vignette.updatePixels()
+        image(vignette, 0f, 0f)
     }
 
     @Synchronized
@@ -58,67 +97,72 @@ class RevealingStripes : BaseSketch(
     }
 
     private fun generateNewStripe() {
-        val segment = random(numOfSegments).toInt()
-        if(revealBackgroundImage) {
-            addCutoutImage(segment)
-        } else {
-//        addColorStripe(segment)
-        }
-    }
-
-    @Synchronized
-    private fun addColorStripe(segment: Int) {
-        stripes[segment] = Stripe(segment, color = colors.random)
+        if (isRevealed) return
+        val segment = segmentOrder[segmentPointer++]
+        addCutoutImage(segment)
     }
 
     @Synchronized
     private fun addCutoutImage(segment: Int) {
         println("showing $segment")
-        if (showMiddleSegmentLast && segment == middleSegment && !haveAllOthersBeenShown()) {
-            println("Should have shown but didn't")
-            return
-        } else {
-            val cutout = getCutout(segment)
-            stripes[segment] = Stripe(segment, image = cutout)
-        }
+        val cutout = getCutout(segment)
+        stripes[segment] = Stripe(segment, image = cutout)
     }
 
-    private fun haveAllOthersBeenShown(): Boolean {
-        return stripes.keys.size >= numOfSegments - 1
+    private fun getCutout(segment: Int): PImage {
+        return originalImage.get((segment * segmentSize), 0, segmentSize, height)
     }
 
-    private fun getCutout(segment: Int): PImage? {
-        return background.get((segment * segmentSize), background.height - height, segmentSize, height)
+    private fun generateSegmentOrder(): List<Int> {
+        return MutableList(numOfSegments) { it }
+            .filter { !middleStrips.contains(it) }
+            .shuffled()
+            .toMutableList()
+            .plus(middleStrips)
     }
 
     private inner class Stripe(
-        val index: Int,
-        val color: Int? = null,
-        val image: PImage? = null
+        val stripeIndex: Int,
+        val image: PImage
     ) {
 
-        var alpha = 355f
+        private var isRevealed = false
+        private var progress = 0f
+        private val offsets = List(segmentSize / subsegmentWidth) {
+            // config
+//            (noise(stripeIndex + it / 2f) * maxSubsegmentOffset).toInt()
+            Random.nextInt(maxSubsegmentOffset)
+        }
 
         fun update() {
-            if (alpha > minAlpha) alpha -= 1f
+            if (!isRevealed && progress < 1f) progress += revealProgressSpeed / 1000f else isRevealed = true
         }
 
         fun draw() {
-            if (color != null) {
-                fill(color, alpha)
-                stroke(white)
-                strokeWeight(2f)
-                rect(index * segmentSize.toFloat(), 0f, segmentSize.toFloat(), heightF)
-            } else if (image != null) {
-                tint(255, alpha)
-                image(image, index * segmentSize.toFloat(), 0f)
-            }
+            if (isRevealed) {
+                image(image, stripeIndex * segmentSize.toFloat(), 0f)
+            } else revealImage(image)
+
             showStripeNumbers()
         }
 
+        private fun revealImage(image: PImage) {
+            val yProgress = map(progress, 0f, 1f, 0f, heightF).toInt()
+            offsets.forEachIndexed { index, offset ->
+                val y = constrain(height - yProgress - offset, 0, height)
+                val progressImage =
+                    image.get(index * subsegmentWidth, y, subsegmentWidth, yProgress + offset)
+                image(
+                    progressImage,
+                    stripeIndex * segmentSize.toFloat() + index * subsegmentWidth,
+                    y.toFloat()
+                )
+            }
+        }
+
         private fun showStripeNumbers() {
-            if(showStripeNumbers)
-             text("$index", index * segmentSize.toFloat() + segmentSize / 2f, heightF - 20f)
+            if (showStripeNumbers)
+                text("$stripeIndex", stripeIndex * segmentSize.toFloat() + segmentSize / 2f, heightF - 20f)
         }
     }
 }
