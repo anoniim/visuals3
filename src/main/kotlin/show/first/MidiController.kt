ad37be19f58d06e9a0e40bbd6583ea41098a958d
Marcel Bro <marcel.broz@gmail.com>
2021-12-28 20:17:26 +0000

    Controller knobs and wider range of keys/pads

--- src/main/kotlin/show/first/MidiController.kt
+++ src/main/kotlin/show/first/MidiController.kt
@@ -16,6 +16,7 @@ class MidiController(applet: PApplet, input: Int, output: Int) {
     private val triggerActions = mutableMapOf<Int, (pitch: Int, velocity: Int) -> Unit>()
     private val releaseActions = mutableMapOf<Int, ((pitch: Int) -> Unit)?>()
     private val activeNotes = MutableList<Note?>(255) { null }
+    private val controllers = mutableMapOf<Int, ((Int) -> Unit)>()
 
     init {
         applet.registerMethod("dispose", this)
@@ -39,6 +40,13 @@ class MidiController(applet: PApplet, input: Int, output: Int) {
     ) {
         triggerActions[button] = triggerAction
         releaseActions[button] = releaseAction
+    }
+
+    fun on(
+        controller: Int,
+        changeAction: (change: Int) -> Unit,
+    ) {
+        controllers[controller] = changeAction
     }
 
     fun isOn(button: Int): Boolean {
@@ -69,6 +77,8 @@ class MidiController(applet: PApplet, input: Int, output: Int) {
      * Called by MidiBus when there is a controller change.
      */
     fun controllerChange(channel: Int, number: Int, value: Int) {
+        controllers[number]?.invoke(value)
+
         PApplet.println("Control $number changed to $value")
     }
 
@@ -105,6 +115,63 @@ class MidiController(applet: PApplet, input: Int, output: Int) {
         const val PAD_22 = 33
         const val PAD_23 = 34
         const val PAD_24 = 35
+        const val PAD_25 = 36
+        const val PAD_26 = 37
+        const val PAD_27 = 38
+        const val PAD_28 = 39
+        const val PAD_29 = 40
+        const val PAD_30 = 41
+        const val PAD_31 = 42
+        const val PAD_32 = 43
+        const val PAD_33 = 44
+        const val PAD_34 = 45
+        const val PAD_35 = 46
+        const val PAD_36 = 47
+        const val PAD_37 = 48
+        const val PAD_38 = 49
+        const val PAD_39 = 50
+        const val PAD_40 = 51
+        const val PAD_41 = 52
+        const val PAD_42 = 53
+        const val PAD_43 = 54
+        const val PAD_44 = 55
+        const val PAD_45 = 56
+        const val PAD_46 = 57
+        const val PAD_47 = 58
+        const val PAD_48 = 59
+        const val PAD_49 = 60
+        const val PAD_50 = 61
+        const val PAD_51 = 62
+        const val PAD_52 = 63
+        const val PAD_53 = 64
+        const val PAD_54 = 65
+        const val PAD_55 = 66
+        const val PAD_56 = 67
+        const val PAD_57 = 68
+        const val PAD_58 = 69
+        const val PAD_59 = 70
+        const val PAD_60 = 71
+        const val PAD_61 = 72
+        const val PAD_62 = 73
+        const val PAD_63 = 74
+        const val PAD_64 = 75
+        const val PAD_65 = 76
+        const val PAD_66 = 77
+        const val PAD_67 = 78
+        const val PAD_68 = 79
+        const val PAD_69 = 80
+        const val PAD_70 = 81
+        const val PAD_71 = 82
+        const val PAD_72 = 83
+        const val PAD_73 = 84
+        const val PAD_74 = 85
+        const val PAD_75 = 86
+        const val PAD_76 = 87
+        const val PAD_77 = 88
+        const val PAD_78 = 89
+        const val PAD_79 = 90
+
+        const val KNOB_14 = 14 // Mikro knob
 
         fun printDevices() {
             MidiBus.list()
