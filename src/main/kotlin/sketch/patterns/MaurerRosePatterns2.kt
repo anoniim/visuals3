8174ff1206a2afd9585ccadf388df0c8a1f604d5
Marcel Bro <marcel.broz@gmail.com>
2022-01-23 18:30:29 +0000

    Progress on Maurer Roses for show * Controller for precise speed change * Export option added * d and granularity values set according to the exported bit

--- src/main/kotlin/sketch/patterns/MaurerRosePatterns2.kt
+++ src/main/kotlin/sketch/patterns/MaurerRosePatterns2.kt
@@ -2,6 +2,7 @@ package sketch.patterns
 
 import BaseSketch
 import Screen
+import com.hamoid.VideoExport
 import processing.core.PApplet
 import input.MidiController
 import util.roundTo
@@ -19,41 +20,69 @@ fun main() {
 
 // under 1 minute
 
+
+// i = 27
+// 68.1
+// 70.0
+// 70.55
+// 71.0
+
 /**
- * TODO Find an interesting n (e.g. 3.66), scan through all 360° and pick most interesting segments that will be slowed down
  * TODO map speed and save controls to Maschine (slow/medium/fast, forward/backward, stop/start, save start, save end)
  * TODO add symbol for speed mode (arrow left/right, color by speed)
  * TODO each line different color -> gradient
  */
 class MaurerRosePatterns2 : BaseSketch(
-    Screen.LG_ULTRAWIDE,
+//    Screen.LG_ULTRAWIDE,
+    Screen(1280, 720),
 ) {
 
-    private val nProgress = ValueProgress(minValue = 2f, maxValue = 8f)
-    private val dProgress = ValueProgress(minValue = 0f, maxValue = 360f)
+    private val dProgress = ValueProgress(minValue = 69.9f, maxValue = 71.0f)
+    private var granularity = 0.2109375f // 1f
 
-    private val scale = 450f
+    private val scale = 350f
     private val textSize = 24f
     private val textScale = 1.3f
     private val stopArea = screen.widthF / 8
     private var direction = 1f
+    private var recordingOn: Boolean = false // record
 
     private val midiController by lazy { MidiController(this, 1, 2) }
+    private val videoExport by lazy { VideoExport(this) }
 
     override fun setup() {
         super.setup()
+        if (recordingOn) videoExport.startMovie()
         MidiController.printDevices()
         midiController.on(36..37,
             triggerAction = { pitch, velocity ->
-                println("Pitch: $pitch")
                 when (pitch) {
                     36 -> direction = -1f
                     37 -> direction = 1f
                 }
-        })
+            })
+        midiController.on(38, { _, _ ->
+            recordingOn = !recordingOn
+        }, null)
         midiController.on(MidiController.CONTROL_1) { value ->
             val change = map(value.toFloat(), 0f, 127f, 0f, 1f)
             dProgress.setIncrement(change)
+        }
+        midiController.on(MidiController.CONTROL_18) { value ->
+            val change = map(value.toFloat(), 0f, 127f, 0f, 0.001f)
+            dProgress.setIncrement(change)
+        }
+        midiController.on(MidiController.CONTROL_19) { value ->
+            val change = map(value.toFloat(), 0f, 127f, 0f, 0.01f)
+            dProgress.setIncrement(change)
+        }
+        midiController.on(MidiController.CONTROL_16) { value ->
+            val change = map(value.toFloat(), 0f, 127f, 0f, 1f)
+            dProgress.setIncrement(change)
+        }
+        midiController.on(MidiController.CONTROL_17) { value ->
+            val change = map(value.toFloat(), 0f, 127f, 0f, 1f)
+            granularity = change
         }
     }
 
@@ -63,32 +92,43 @@ class MaurerRosePatterns2 : BaseSketch(
         background(grey3)
 //        val (n, d) = mousePressControl()
         val d = dProgress.last
-        val n = 2.42f
+        val n = 12.42f
         drawRoseShape(n, d)
+        dProgress.progress()
+
+        manageRecording()
         drawValueText(n, d)
 //        drawControlGuidelines()
-        dProgress.progress()
     }
 
-    private fun drawControlGuidelines() {
-        line(-stopArea, -halfHeightF, -stopArea, -halfHeightF + 20f)
-        line(stopArea, -halfHeightF, stopArea, -halfHeightF + 20f)
+    private fun manageRecording() {
+        videoExport.saveFrame()
+        strokeWeight(20f)
+        stroke(red)
+        point(-halfWidthF + 20f, -halfHeightF + 20f)
     }
 
     private fun drawRoseShape(n: Float, d: Float) {
         noFill()
-        strokeWeight(4f)
-        stroke(grey9)
+        strokeWeight(1f)
+        stroke(white)
         beginShape()
         var i = 0f
         while (i <= d) {
-            val radius = scale * sin(radians(n * i * d))
-            val x = radius * cos(radians(i * d))
-            val y = radius * sin(radians(i * d))
-            curveVertex(x, y)
-            i += 1f
+            addVertex(n, i, d)
+            i += granularity
         }
+//        addVertex(n, 0f, d)
+//        addVertex(n, granularity, d)
+//        addVertex(n, 2 * granularity, d)
         endShape()
+    }
+
+    private fun addVertex(n: Float, i: Float, d: Float) {
+        val radius = scale * sin(radians(n * i * d))
+        val x = radius * cos(radians(i * d))
+        val y = radius * sin(radians(i * d))
+        curveVertex(x, y)
     }
 
     private fun drawValueText(n: Float, d: Float) {
@@ -98,22 +138,11 @@ class MaurerRosePatterns2 : BaseSketch(
         text("d: $d", -widthF / 2 + textSize * textScale, -heightF / 2 + 2 * (textSize * textScale))
     }
 
-    private fun mousePressControl(): Pair<Float, Float> {
-        if (mousePressed) {
-            if (mouseY < height / 2) {
-                nProgress.progress()
-            } else {
-                dProgress.progress()
-            }
-        }
-        return Pair(nProgress.last, dProgress.last)
-    }
-
     inner class ValueProgress(val minValue: Float, val maxValue: Float) {
 
         private val maxLoopTime: Float = 10000f // 1000f
-        private val numDecimalPoints = 2 // 3
-        private var increment = 0.5f
+        private val numDecimalPoints = 5 // 3
+        private var increment = 3f
 
         var last = 0f
         private var value = 0f
@@ -148,7 +177,7 @@ class MaurerRosePatterns2 : BaseSketch(
         }
 
         fun setIncrement(change: Float) {
-            increment = direction * incrementFromKnob(change)
+            increment = direction * change
         }
     }
 }
